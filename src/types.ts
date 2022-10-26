0e23c7c21f397ba12d1fbc711482b0c6af551f9c
PaquierThomas <95091416+PaquierThomas@users.noreply.github.com>
2022-10-26 09:21:32 +0200

    ajout type ts (montre et colors)

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export interface Montre {
+  boitier?: string;
+  bracelet?: string;
+  ecran?: string;
+}
+export const colors = {
+  "#000000": "Noir",
+  "#FFFFFF": "Blanc",
+  "#FF0000": "Rouge",
+  "#00FF00": "Vert",
+  "#0000FF": "Bleu",
+  "#FFFF00": "Jaune",
+  "#FF00FF": "Magenta",
+  "#00FFFF": "Cyan",
+  "#C0C0C0": "Gris clair",
+};
